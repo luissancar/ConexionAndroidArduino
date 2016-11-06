c0b8973ee8fce9fca29cbe7228217a7f64810bcd
luissancar <luissancar@yahoo.es>
2016-11-06 13:15:41 +0100

    Initial commit

---
+++ app/src/main/java/com/example/luissancar/conexionandroidarduino/Temperatura.java
@@ -0,0 +1,39 @@
+package com.example.luissancar.conexionandroidarduino;
+
+/**
+ * Created by luissancar on 6/11/16.
+ */
+
+
+
+public class Temperatura {
+
+    private String informacion = "";
+    private String temperatura = "";
+    private String nombreDispositivo = "";
+
+    public String getInformacion() {
+        return informacion;
+    }
+
+    public void setInformacion(String informacion) {
+        this.informacion = informacion;
+    }
+
+    public String getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(String temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    public String getNombreDispositivo() {
+        return nombreDispositivo;
+    }
+
+    public void setNombreDispositivo(String nodo) {
+        this.nombreDispositivo = nodo;
+    }
+}
+
